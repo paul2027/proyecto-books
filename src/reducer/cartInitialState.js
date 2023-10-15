13ed0c98bb94d3a7456dff8edc678238406ff155
paulfernandez <paulfernandez4444@gmail.com>
2023-10-15 03:06:18 -0300

    shoppingCart

---
+++ src/reducer/cartInitialState.js
@@ -0,0 +1,17 @@
+
+export const cartInitialState = {
+
+    products :[  { id: 1 ,nombre: "producto A" ,precio :200 },
+                 { id: 2 ,nombre: "producto B" ,precio :300 },
+                 { id: 3 ,nombre: "producto C" ,precio :400 },
+                 { id: 4 ,nombre: "producto D" ,precio :500 },
+                 { id: 5 ,nombre: "producto E" ,precio :600 },
+                ],
+            
+    cart : [
+],
+            
+            }
+
+
+
