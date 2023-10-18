84c9f5d43245310359ab7a63107f32f9bf2a9fa1
rocaillava <rocaillava@gmail.com>
2023-10-18 19:12:25 -0300

    MenuMobile

--- src/molecules/Header.js
+++ src/molecules/Header.js
@@ -1,10 +1,11 @@
+import useMediaQuery from "@/hooks/useMediaQuery"
 import { roboto_Mono } from "@/pages/_app"
 import Image from "next/image"
 import Link from "next/link"
 
 
 const Header = () => {
-
+const ismobile=useMediaQuery(770)
 
 
   return (
@@ -14,12 +15,22 @@ const Header = () => {
     </div>
     <div className="Navbar">
     <Image
+    className="imagenLogo"
       src="/logo.png"
       width={300}
       height={81}
       alt="Logo Books"
       />
- <div className="Buscador">
+      <div className="menuMobile">
+        {ismobile &&(
+          <Image
+          src="/hamburguesa_verde.png"
+          width={70}
+          height={53}
+          alt="Logo Books"
+          />
+        ) }
+        <div className="Buscador">
     <input className="Barrabusqueda"/>
     <Image
       src="/lupa.png"
@@ -28,20 +39,28 @@ const Header = () => {
       alt="Logo Books"
       />
     </div>
-    <Image
-      src="/iconos.png"
-      width={200}
-      height={54}
-      alt="Logo Books"
-      />
+      </div>
+ 
+    {!ismobile && (
+       <Image
+       src="/iconos.png"
+       width={200}
+       height={54}
+       alt="Logo Books"
+       />
+    )}
     </div>
-    <div className="Menu">
-        <Link className="link-menu" href="#categorias">CATEGORÍAS</Link>
-        <Link className="link-menu" href="#best-seller">BEST SELLER</Link>
-        <Link className="link-menu" href="#ficcion">FICCIÓN</Link>
-        <Link className="link-menu" href="#clasicos">CLÁSICOS</Link>
-        <Link className="link-menu" href="#contacto">CONTACTO</Link>
-    </div>
+    {!ismobile && (
+      <div className="Menu">
+      <Link className="link-menu" href="#categorias">CATEGORÍAS</Link>
+      <Link className="link-menu" href="#best-seller">BEST SELLER</Link>
+      <Link className="link-menu" href="#ficcion">FICCIÓN</Link>
+      <Link className="link-menu" href="#clasicos">CLÁSICOS</Link>
+      <Link className="link-menu" href="#contacto">CONTACTO</Link>
+  </div>
+
+    )}
+    
    
     
 
