9a17688be06aa35ac344dbd84d2bc174bc57e179
paulfernandez <paulfernandez4444@gmail.com>
2023-11-15 16:54:07 -0300

    arreglo vista del carrito modo mobile

--- src/molecules/CartItems.js
+++ src/molecules/CartItems.js
@@ -2,7 +2,7 @@
 import { cartInitialState } from "@/reducer/cartInitialState";
 import { cartReducer } from "@/reducer/cartReducer";
 
-const CartItems = ({item, deleteFfromCart}) => {
+const CartItems = ({item, deleteFromCart}) => {
 
     const {id, nombre , precio , quantity} = item;
 
@@ -12,8 +12,8 @@ const CartItems = ({item, deleteFfromCart}) => {
     <div className="cart-item">
         <h4>{nombre}</h4>
         <h5>{precio} x {quantity} = { precio * quantity}</h5>
-        <button onClick={() => deleteFfromCart(id)}>Eliminar uno</button>
-        <button onClick={() => deleteFfromCart(id)}>Eliminar todos</button>
+        <button onClick={() => deleteFromCart(id)}>Eliminar uno</button>
+        <button onClick={() => deleteFromCart(id)}>Eliminar todos</button>
     </div>
   )
 }
